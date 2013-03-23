dec72c269254691d5444721e16781abbff81ef30
Kit Farman <suteru@gmail.com>
2013-03-23 15:30:03 -0400

    Updated
    Includes new flyout menu, information on Frogout, changed speed.

--- Get Out!/js/default.js
+++ Get Out!/js/default.js
@@ -31,4 +31,16 @@
     };
 
     app.start();
+	
+    app.onactivated = function scenario3AddSettingsFlyout() { 
+        WinJS.Application.onsettings = function (e) { 
+            e.detail.applicationcommands = { "about": { title: "About Frogsay", href: "default.html" } }; 
+            WinJS.UI.SettingsFlyout.populateSettings(e); 
+        }; 
+        // Make sure the following is called after the DOM has initialized. Typically this would be part of app initialization 
+        WinJS.Application.start(); 
+ 
+        // Display a status message in the SDK sample output region 
+        WinJS.log && WinJS.log("Legal notices command and settings flyout added from 3-SettingsFlyout-Legal.html", "samples", "status"); 
+    };
 })();
